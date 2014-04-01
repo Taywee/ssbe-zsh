885bb29fc4a375bc27777ae8afaf88495f441621
Taylor C. Richberger <taywee@gmx.com>
2014-04-01 02:23:41 -0600

    extra additions to configuration file

--- maketraplogconfigs.sh
+++ maketraplogconfigs.sh
@@ -2,13 +2,20 @@
 # Takes in from standard in a list of host ip pairs.  The hostname must NOT have spaces.  Neither must the IP.
 # Optionally, the script may take one argument, for a script to run on the poller host to create and configure
 # the agents
-# SSBE_USER, SSBE_PASS, AGENT_PASS, and CLIENT are expected to be set as environment variables, otherwise this probably won't work out for you
+# SSBE_USER, SSBE_PASS, AGENT_PASS, and CLIENT are expected to be set as environment variables  (or set in .ssbe), otherwise this probably won't work out for you
+
+if [ -e ./.ssbe ]
+then
+    source ./.ssbe
+fi
 
 if [ -z "${CLIENT}" ]; then CLIENT=omm-location; fi
 
 exec 3>>"${1:-/dev/null}"
 
+echo "cd /opt/SysShep" >&3
 echo "tar -xvzf SysShep-s-UBUNTU-5.3.7.tar.gz" >&3
+echo "export LD_LIBRARY_PATH=../lib" >&3
 echo >&3
 
 while read line
@@ -24,15 +31,17 @@ do
     # Get the config URL
     CONFIG="$(echo "$RESPONSE" | sed -e 's/^.*="//' -e 's/".*$//')"
 
+    echo "Config:  $CONFIG"
+
     http --ignore-stdin -f -a "${SSBE_USER}:${SSBE_PASS}" PUT "${CONFIG}/registered_templates/1302/update_answers" "submit=save" "answers[log-file-regex]=^.*?(?:${HOST}|${FILTEREDIP}).*$" </dev/null
 
     cat >&3 <<HERE
 # ${HOST}-trap ${CONFIG}
 cd /opt/SysShep
-tar -xvzf SysShep-s-UBUNTU-5.3.7.tar.gz
 cp -rv SysShep-s-UBUNTU-5.3.7 ${HOST}-trap
 cd ${HOST}-trap/bin
-./configure "${SSBE_USER}" "${SSBE_PASS}"  ssbe06.qwest.sysshep.com ${CLIENT} ${HOST}-trap '${CONFIG}' '${AGENT_PASS}'
+./configure "${SSBE_USER}" "${SSBE_PASS}"  ssbe06.qwest.sysshep.com ${CLIENT} ${HOST}-trap '${CONFIG}' '${AGENT_PASS}' && ./sysshepctl start
+
 HERE
 
 done
